c22c63859fdd6f0044d689377429a9e2cdc90f27
unknown <jdsjeo@gmail.com git config --global user.name azhar>
2019-07-02 05:53:00 +0700

    first commit

---
+++ precache-manifest.2d672897661fbb47e6117fe861f1598c.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "url": "/static/6896e11e4327a8bb48bc.js"
+  },
+  {
+    "url": "/static/media/c7578911196974808febf223f05a8364.png"
+  },
+  {
+    "url": "/static/media/6165c9d7a2e729ba57b23dd93add5366.png"
+  },
+  {
+    "url": "/static/media/761a29cc5b9163dd2a23118c3d85605d.png"
+  },
+  {
+    "url": "/static/media/54da1e9816c77e30ebc5920e256736f2.png"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e5a9f01fb8766b17becf3d58c448f2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  }
+];
